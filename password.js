7e6ad9d0e49ca5044b7202f4e920ee55490bf6c2
cpuSonicatt <18102974+ColSonicatt@users.noreply.github.com>
2021-08-04 14:09:16 +0100

    Improve passwords

--- password.js
+++ password.js
@@ -1,13 +1,20 @@
-const PASSWORDS = [
-    "ABOUT", "AFTER", "AGAIN", "BELOW",
-    "COULD", "EVERY", "FIRST", "FOUND",
-    "GREAT", "HOUSE", "LARGE", "LEARN",
-    "NEVER", "OTHER", "PLACE", "PLANT",
-    "POINT", "RIGHT", "SMALL", "SOUND",
-    "SPELL", "STILL", "STUDY", "THEIR",
-    "THERE", "THESE", "THING", "THINK",
-    "THREE", "WATER", "WHERE", "WHICH",
-    "WORLD", "WOULD", "WRITE"
+const PASSWORDS_LIST = [
+    "ABOUT", "AFTER", "AGAIN",
+    "BELOW",
+    "COULD",
+    "EVERY",
+    "FIRST", "FOUND",
+    "GREAT",
+    "HOUSE",
+    "LARGE", "LEARN",
+    "NEVER",
+    "OTHER",
+    "PLACE", "PLANT", "POINT",
+    "RIGHT",
+    "SMALL", "SOUND",
+    "SPELL", "STILL", "STUDY",
+    "THEIR", "THERE", "THESE", "THING", "THINK", "THREE",
+    "WATER", "WHERE", "WHICH", "WORLD", "WOULD", "WRITE"
 ]
 
 class Password { 
@@ -18,17 +25,17 @@ class Password {
 
         for (let row of letters) {
             let rowArr = row.split("")
-            PASSWORDS = PASSWORDS.filter(word => {
+            let passwords = PASSWORDS_LIST.filter(word => {
                 for (let x = 0; x <= 5; x++)
                     return rowArr[x] === word.charAt(index)
                 })
             index++
-            if (PASSWORDS.length == 1) {
-                return PASSWORDS[0]
+            if (passwords.length == 1) {
+                return passwords[0]
             }
         }
 
-        throw "Expected 1 result, got " + PASSWORDS
+        throw "Expected 1 result, got " + passwords
     }
 }
 
